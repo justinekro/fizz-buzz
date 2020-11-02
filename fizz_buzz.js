b4dc2200b577d5f21a1d9356c1a300095bb96ea2
Justine <justinekronovsek@gmail.com>
2020-11-02 14:48:54 +0100

    should print Buzz when number is a multiple of 5 : test ok

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -7,6 +7,8 @@ function FizzBuzz() {
 function checkModulo(i) {
 	if (i % 3 === 0) {
 		return "Fizz";
+	} else if (i % 5 === 0) {
+		return "Buzz";
 	}
 }
 
--- test.js
+++ test.js
@@ -3,3 +3,8 @@ const checkModulo = require("./fizz_buzz");
 test("program prints Fizz when the number is a multiple of 3", () => {
 	expect(checkModulo(3)).toBe("Fizz");
 });
+
+test("program prints Buzz when the number is a multiple of 5", () => {
+	expect(checkModulo(10)).toBe("Buzz");
+	expect(checkModulo(5)).toBe("Buzz");
+});
